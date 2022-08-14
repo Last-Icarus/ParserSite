f2f9c75d1e4cc286037821947766e2f2b271c079
Last-Icarus <lordprotector02@gmail.com>
2022-08-14 05:25:26 +0300

    V0.2

--- OriginParser.py
+++ OriginParser.py
@@ -1,69 +1,72 @@
-from itertools import count
 from bs4 import BeautifulSoup
 import requests
 import pandas 
 
 def save():
-    with open('C:/Users/MihaNik/Desktop/VsCodeCode/projects/main.html', 'r') as file:
+    df.to_html('main.html',index=False)
+    with open('main.html','r') as file:
         filedata = file.read()
-        filedata = filedata.replace('&lt;','<').replace('&rt;','>')
-    with open('C:/Users/MihaNik/Desktop/VsCodeCode/projects/main.html', 'w') as file:
+        filedata = filedata.replace('&lt;','<').replace('&rt;','>') # Питон не понимает скобки, приходится заменять их прямо в файле
+    with open('main.html','w') as file:
         file.write(filedata)
 
+def sort(sort_by=None,asc=False):
+    if sort_by == None:
+        return
+    global df                                                       # global позволяет манипулировать таблицей на всех уровнях
+    column_list = []
+    if sort_by=='NAME':                                             # Случай, когда у нас идёт сортировка по названию
+        df = df.sort_values('NAME',ascending=asc)
+        return
+    for i in df[sort_by]:
+        try: 
+            column_list.append(float(i.replace(',','')))            # Сортировка чисел. Весь этот модуль существует только потому, что числа записываются через чёртову запятую
+        except(ValueError):                                         # Но что поделать, так красиво
+            column_list.append(0)                                   # Некоторые предметы не имеют стоимости, тогда вместо ошибки значения им будет присвоен ноль
+    df['SortCol']=column_list
+    df = df.sort_values('SortCol',ascending=asc)
+    del df['SortCol']
+
+def merge():
+    soup = BeautifulSoup(open('Origin.html'),'lxml')                 # Обёртка HTML
+    a = BeautifulSoup(open('main.html'),'lxml')                      # Сама таблица, переконвертированная в HTML
+    soup.body.append(a)                                              # Объединение файлов
+    with open("child.html", "w") as file:                            # Запись нового файла
+        file.write(str(soup))
+
+def main():
+    url = 'https://www.thecycledb.com/items'
+    header = ['NAME','LOGO','K-MARKS VALUE','VALUE PER WEIGHT','FACTION XP','XP PER POUND','WEIGHT','PURCHASE/CRAFT COST']
+    soup = BeautifulSoup(requests.get(url).text,'lxml')
+
+    items = []
+
+    for item in soup.find_all('tr',class_='hover cursor-pointer group hover:active'):
+        list=[item.find('th').text]
+        s = soup.find_all('img',alt=list[0])[1]['src']
+        list.append(f'<img src="{s}" style="height:70px">')
+        for element in item.find_all('td')[0:5]:
+            list.append(element.text)
+
+        item_soup = BeautifulSoup(requests.get(url[:-1]+'/'+list[0].lower().replace("'",'').replace(' ','-')).text,'lxml')  # Парсинг рыночной стоимости с заходом на страницу предмета
+        try:
+            a = item_soup.find('h3',string='Shop Price')
+            list.append(a.next_element.next_element.text)
+        except(AttributeError):
+            list.append("Can't be bought")
+        items.append(list)
+
+    global df                                                        # global позволяет манипулировать таблицей на всех уровнях
+    df = pandas.DataFrame(items,columns=header)
+    columns_titles = ['LOGO','NAME','K-MARKS VALUE','VALUE PER WEIGHT','FACTION XP','XP PER POUND','WEIGHT','PURCHASE/CRAFT COST']
+    df = df.reindex(columns=columns_titles)
 
 
-# Main parser module
+def compile(sort_by=None): 
+    main()
+    sort(sort_by)
+    save()
+    merge()
 
-url = 'https://www.thecycledb.com/items'
-header = ['NAME','LOGO','K-MARKS VALUE','VALUE PER WEIGHT','FACTION XP','XP PER POUND','WEIGHT','PURCHASE/CRAFT COST']
-
-soup = BeautifulSoup(requests.get(url).text,'lxml')
-
-items = []
-counter = 0
-
-for item in soup.find_all('tr',class_='hover cursor-pointer group hover:active'):
-    list=[item.find('th').text]
-    s = soup.find_all('img',alt=list[0])[1]['src']
-    list.append(f'<img src="{s}" style="height:70px">')
-    for element in item.find_all('td')[0:5]:
-        list.append(element.text)
-    item_soup = BeautifulSoup(requests.get(url[:-1]+'/'+list[0].lower().replace("'",'').replace(' ','-')).text,'lxml')
-    try:
-        a = item_soup.find('h3',string='Shop Price')
-        list.append(a.next_element.next_element.text)
-    except(AttributeError):
-        list.append("Can't be bought")
-        print(url[:-1]+'/'+list[0].lower())
-
-    items.append(list)
-    counter += 1
-    if counter == 10:
-        break
-
-
-df = pandas.DataFrame(items,columns=header)
-
-columns_titles = ['LOGO','NAME','K-MARKS VALUE','VALUE PER WEIGHT','FACTION XP','XP PER POUND','WEIGHT','PURCHASE/CRAFT COST']
-df = df.reindex(columns=columns_titles)
-df.to_html('C:/Users/MihaNik/Desktop/VsCodeCode/projects/main.html')
-
-save()
-
-#----------------------------------------------------------------------------------------------------------------------
-
-
-
-
-soup = BeautifulSoup(open('C:/Users/MihaNik/Desktop/VsCodeCode/Projects/Origin.html'),'lxml')
-
-a = BeautifulSoup(open('C:/Users/MihaNik/Desktop/VsCodeCode/Projects/main.html'),'lxml')
-
-
-soup.body.append(a)
-
-
-
-with open("C:/Users/MihaNik/Desktop/VsCodeCode/Projects/child.html", "w") as file:
-    file.write(str(soup))
-#print(soup.prettify())
+if __name__ == '__main__':
+    compile()
