943c9d7c56548ce4f402ef91bc0f81e23b27c0d5
Last-Icarus <lordprotector02@gmail.com>
2022-08-14 21:48:12 +0300

    V0.3

--- OriginParser.py
+++ OriginParser.py
@@ -3,71 +3,88 @@ import requests
 import pandas 
 
 
+class Frame():
+    def __init__(self,df):
+        self.df = df
 
-def save():
-    df.to_html('main.html',index=False)
-    with open('main.html','r') as file:
-        filedata = file.read()
-        filedata = filedata.replace('&lt;','<').replace('&gt;','>') # Питон не понимает скобки, приходится заменять их прямо в файле
-    with open('main.html','w') as file:
-        file.write(filedata)
+    def main(self,count=0):
 
-def merge():
-    soup = BeautifulSoup(open('Origin.html'),'lxml')                 # Обёртка HTML
-    a = BeautifulSoup(open('main.html'),'lxml')                      # Сама таблица, переконвертированная в HTML
-    soup.body.append(a)                                              # Объединение файлов
-    with open("index.html", "w") as file:                            # Запись нового файла
-        file.write(str(soup))
+        # Шапка и запрос сайта с таблицей
 
-def main(count=5):
-    url = 'https://www.thecycledb.com/items'
-    header = ['NAME','LOGO','K-MARKS VALUE','VALUE PER WEIGHT','FACTION XP','XP PER POUND','WEIGHT','PURCHASE/CRAFT COST']
-    soup = BeautifulSoup(requests.get(url).text,'lxml')
+        url = 'https://www.thecycledb.com/items'
+        header = ['NAME','LOGO','K-MARKS VALUE','VALUE PER POUND','FACTION XP','XP PER POUND','WEIGHT','PURCHASE/CRAFT COST']
+        soup = BeautifulSoup(requests.get(url).text,'lxml')
 
-    items = []
-    #-------------
-    counter = 0
-    #-------------
+        items = []
 
-    for item in soup.find_all('tr',class_='hover cursor-pointer group hover:active'):
-        list=[item.find('th').text]
-        s = soup.find_all('img',alt=list[0])[1]['src']
-        list.append(f'<img src="{s}" style="height:70px">')
-        for element in item.find_all('td')[0:5]:
-            a = float(element.text.replace(',',''))
-            if a % 1 == 0:
-                a = int(a)
-            list.append(a)
+        #-------------
+        counter = 0
+        #-------------
 
-        item_soup = BeautifulSoup(requests.get(url[:-1]+'/'+list[0].lower().replace("'",'').replace(' ','-')).text,'lxml')  # Парсинг рыночной стоимости с заходом на страницу предмета
-        try:
-            a = item_soup.find('h3',string='Shop Price')
-            list.append(a.next_element.next_element.text.replace(',',''))
-        except(AttributeError):
-            list.append("Can't be bought")
-        items.append(list)
-    #-------------
-        counter+=1
-        if counter==count:
-            break
-    #-------------
+        # Основной модуль парсинга
 
-    global df                                                         # global позволяет манипулировать таблицей на всех уровнях(Заглушка, потом реализую через ООП)
-    df = pandas.DataFrame(items,columns=header)
-    pandas.options.display.float_format = '{:,}'.format
-    header = ['LOGO','NAME','K-MARKS VALUE','VALUE PER WEIGHT','FACTION XP','XP PER POUND','WEIGHT','PURCHASE/CRAFT COST']
-    df = df.reindex(columns=header)
+        for item in soup.find_all('tr',class_='hover cursor-pointer group hover:active'):
+            list=[item.find('th').text]                             # Парсинг имени
+
+            s = soup.find_all('img',alt=list[0])[1]['src']          # Парсинг иконки по имени
+            list.append(f'<img src="{s}" style="height:70px">')
+
+            for element in item.find_all('td')[0:5]:                # Парсинг остальных элементов кроме последнего
+                a = float(element.text.replace(',',''))
+                if a % 1 == 0:
+                    a = int(a)
+                list.append(a)
+
+            item_soup = BeautifulSoup(requests.get(url[:-1]+'/'+list[0].lower().replace("'",'').replace(' ','-')).text,'lxml')  # Парсинг последнего эелемента со страницы предмета
+            try:
+                a = item_soup.find('h3',string='Shop Price')
+                list.append(int(a.next_element.next_element.text.replace(',','')))
+            except(AttributeError):
+                list.append(0)
+            items.append(list)
+        #-------------
+            counter+=1
+            if counter==count:
+                break
+        #-------------
+
+        # Сохранение 
+
+        self.df = pandas.DataFrame(items,columns=header,dtype=object) # dtype для того, чтобы столб воспринимал одновременно int и float, иначе будет автоконвертация
+        pandas.options.display.float_format = '{:,}'.format # Сделал так, чтобы тысячи отделялись запятой, потому что так красивее
+
+        header = ['LOGO','NAME','K-MARKS VALUE','VALUE PER POUND','FACTION XP','XP PER POUND','WEIGHT','PURCHASE/CRAFT COST']  # Поменял местами картинки и названия, потому что так красивее
+        self.df = self.df.reindex(columns=header)
+
+    def save(self):
+        self.df['PURCHASE/CRAFT COST'] = self.df['PURCHASE/CRAFT COST'].replace(0,'Cannot be bought',regex=True) # Изначально поставил нули, т.к. Pandas не может сравнивать числа и строки. Теперь меняю их а что-то покрасивее
+        self.df.to_html('main.html',index=False) # Конвертировал в HTML и выключил индексы потому что зачем они нужны
+
+        with open('main.html','r') as file: # Питон не понимает скобки, приходится заменять их прямо в файле, если бы не это, можно было бы использовать на один файл меньше
+            filedata = file.read()
+            filedata = filedata.replace('&lt;','<').replace('&gt;','>') 
+        with open('main.html','w') as file:
+            file.write(filedata)
+
+    def merge(self):
+        soup = BeautifulSoup(open('Origin.html'),'lxml')                 # Обёртка HTML, прописанная отдельно
+        a = BeautifulSoup(open('main.html'),'lxml')                      # Сама таблица, конвертированная в HTML
+        soup.body.append(a)                                              # Объединение файлов
+        with open("index.html", "w") as file:                            # Запись нового файла
+            file.write(str(soup))
 
 
-def compile(sort_by=None,asc=False): 
-    main(6)
-    if sort_by != None:
-        global df
-        df = df.sort_values(sort_by,ascending=asc)
-    save()
-    merge()
+    def compile(self,sort_by=None,asc=False,items=5): 
+        self.main(items)
+        if sort_by != None:
+            self.df = self.df.sort_values(sort_by,ascending=asc)
+        self.save()
+        self.merge()
+
+
 
 if __name__ == '__main__':
-    compile('K-MARKS VALUE')
+    obj = Frame(None)
+    obj.compile('VALUE PER POUND',items=10,asc=False)
 
 
